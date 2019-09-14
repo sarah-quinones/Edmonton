0a1a76dc917a699311180ebe5686073e0e3d450c
Rietty <rie.juliette@gmail.com>
2019-09-13 20:02:03 -0400

    Finished problem 10.

--- 10/Solution.cpp
+++ 10/Solution.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <numeric>
 
 using namespace std;
 
 // When checking for prime numbers we only need to go till sqrt(n).
-constexpr bool is_prime(int n) {
+constexpr bool is_prime(long n) {
     for(int i = 2; i <= sqrt(n); i++) {
         if(n % i == 0) {
             return false;
@@ -15,14 +16,15 @@ constexpr bool is_prime(int n) {
 }
 
 int main(int argc, char* argv[]) {
-    // Put everything in vector and stop when the size is 10001.
-    vector<int> primes;
-    int i = 2;
-    while(primes.size() != 10'001) {
+    // Put everything in vector and stop when we hit 2,000,000 on current integer. 
+    vector<long> primes;
+    long i = 2;
+    while(i < 2'000'000) {
         if(is_prime(i++)) {
             primes.push_back(i - 1);
         }
     }
-    cout << primes[primes.size() - 1] << endl;
+    // Auto sum up result and print it for us.
+    cout << accumulate(primes.begin(), primes.end(), (long)0) << endl;
     return 0;
 }
