0a85acc67aab2a954b76d94bda609b5ec0931cc1
Rietty <rie.juliette@gmail.com>
2019-09-14 18:50:46 -0400

    Finished problem 22.

---
+++ 22/Solution.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+// Function that returns the letter score of a given word.
+int letterScore(string_view s) {
+    int sum = 0;
+    for(unsigned int i = 0; i < s.length(); i++) {
+        sum += s[i] - 'A' + 1;
+    }
+    return sum;
+}
+
+int main(int argc, char* argv[]) {
+    // Read in the data file into the vector.
+    ifstream input("Data.txt");
+    vector<string> names;
+    istream_iterator<string> end, start(input);
+    copy(start, end, back_inserter(names));
+    sort(names.begin(), names.end());
+    
+    // Go through the whole vector and sum up the scores.
+    int list_sum = 0;
+    for(unsigned int i = 0; i < names.size(); i++) {
+        list_sum += ((i + 1) * letterScore(names[i]));
+    }
+
+    cout << list_sum << endl;
+    return 0;
+}   
