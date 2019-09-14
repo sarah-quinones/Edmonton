348fc5355ae1267a1413e726bb097598e7bb57ce
Rietty <rie.juliette@gmail.com>
2019-09-14 11:21:17 -0400

    Finished problem 13.

---
+++ 13/Solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace std;
+using boost::multiprecision::cpp_int;
+
+int main(int argc, char* argv[]) {
+    // Read in the data file into the vector.
+    ifstream input("Data.txt");
+    vector<cpp_int> numbers;
+    istream_iterator<cpp_int> end, start(input);
+    copy(start, end, back_inserter(numbers));
+
+    // Accumulate the result and put it into a variable.
+    cpp_int result = accumulate(numbers.begin(), numbers.end(), (cpp_int) 0);
+
+    // Print the first 10 digits.
+    while (cpp_int (result / (cpp_int)pow(10, 10)) != 0) { 
+        result = result / 10; 
+    }
+    cout << result << endl;
+    return 0;
+}   
--- 15/Solution.cpp
+++ 15/Solution.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <map>
 #include <boost/multiprecision/cpp_int.hpp>
 
 using namespace std;
