626256b2b33a67c00cf40e9942bcb74f6167099a
Rietty <rie.juliette@gmail.com>
2019-09-15 10:44:42 -0400

    Finished problem 29.

---
+++ Solutions/29/Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace std;
+using boost::multiprecision::cpp_int;
+
+int main(int argc, char* argv[]) {
+    // Sets only hold unique integers, making it the perfect container.
+    set<cpp_int> numbers;
+    for(cpp_int a = 2; a < 101; a++) {
+        for(int b = 2; b < 101; b++) {
+            numbers.emplace(boost::multiprecision::pow(a, b));
+        }
+    }
+    cout << numbers.size() << endl;
+    return 0;
+}
