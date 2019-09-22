61b43f02347dfe9b23711087c09cf16b2618c109
Rietty <rie.juliette@gmail.com>
2019-09-21 22:22:32 -0400

    Finished problem 52.

---
+++ Solutions/52/Solution.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	// This problem is fairly easy. We can get each multiple, convert it to a string and then compare all the strings.
+	// The bounds for this are given because we will have 6 digits and need each one to be unique.
+	for(int i = 100'000; i <= 166'000; i++) {
+		vector<string> nums;
+		nums.push_back(to_string(i));
+		nums.push_back(to_string(i * 2));
+		nums.push_back(to_string(i * 3));
+		nums.push_back(to_string(i * 4));
+		nums.push_back(to_string(i * 5));
+		nums.push_back(to_string(i * 6));
+		for(auto &n : nums) {
+			sort(n.begin(), n.end());
+		}
+		if(nums[0] == nums[1] && nums[1] == nums[2] && nums[2] == nums[3] && nums[3] == nums[4] && nums[4] == nums[5]) {
+			cout << i << endl;
+			break;
+		}
+	}
+	return 0;
+}
