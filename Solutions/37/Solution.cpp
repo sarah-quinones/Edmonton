62a9517abbe4042f89fcad1ff736736dd4caf658
Rietty <rie.juliette@gmail.com>
2019-09-22 16:19:42 -0400

    Finished problem 37.

---
+++ Solutions/37/Solution.cpp
@@ -0,0 +1,30 @@
+#include "../../Headers/Edmonton.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr bool isTrunctable(int n, vector<bool> &primes) {
+	for(int i = 10; i < n; i *= 10) {
+		if(!primes[n % i]) {
+			return false;
+		}
+	}
+	for(; n; n /= 10) {
+		if(!primes[n]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	vector<bool> primes = Edmonton::primesUpto(1'000'000);
+	int sum = 0;
+	for(int n = 11; n < primes.size(); n += 2) {
+		if(isTrunctable(n, primes)) {
+			sum += n;
+		}
+	}
+	std::cout << sum << "\n";
+}
