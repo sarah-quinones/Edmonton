4f5a376a3868ec6744bbf8299dfac87d6efade5d
Rietty <rie.juliette@gmail.com>
2019-09-22 13:37:05 -0400

    Updated header comments.

--- Headers/Edmonton.hpp
+++ Headers/Edmonton.hpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <cmath>
+#include <utility>
 #include <vector>
 
 // A collection of useful functions that I came up with when solving these problems and that are used often.
@@ -37,7 +39,7 @@ namespace Edmonton {
 		return true;
 	}
 
-	// Generate the first N primes using the Sieve of Eratosthenes.
+	// Generate the primes upto n using the Sieve of Eratosthenes.
 	template<typename T>
 	std::vector<T> generatePrimes(T n) {
 		std::vector<T> result = std::vector<T>();
@@ -77,4 +79,25 @@ namespace Edmonton {
 		}
 		return true;
 	}
-}
+
+	/*
+	Generic function to find an element in vector and also its position.
+	It returns a pair of bool & int i.e.
+	bool : Represents if element is present in vector or not.
+	int : Represents the index of element in vector if its found else -1
+	*/
+	template<typename T>
+	std::pair<bool, int> findInVector(const std::vector<T> &vecOfElements, const T &element) {
+		std::pair<bool, int> result;
+		// Find given element in vector
+		auto it = std::find(vecOfElements.begin(), vecOfElements.end(), element);
+		if(it != vecOfElements.end()) {
+			result.second = distance(vecOfElements.begin(), it);
+			result.first = true;
+		} else {
+			result.first = false;
+			result.second = -1;
+		}
+		return result;
+	}
+} // namespace Edmonton
