5ddf15c33bd67b58b37b7df7d74587d6898275f0
Rietty <rie.juliette@gmail.com>
2019-09-21 23:40:59 -0400

    Fixed typo.

--- Solutions/85/Solution.cpp
+++ Solutions/85/Solution.cpp
@@ -10,7 +10,7 @@ If we draw this all out on paper using for example a 5 x 4 grid we can get a sim
 Rectangles are essentially a bunch of parallel lines. If there are M and N lines, we have L + 1 and W + 1 rectangles.
 We need to essentially get the total combination of each type of line and multiply it out.
 
-So it essentially becomes: (l * N * (l + 1) * (w + 1)) / 4.
+So it essentially becomes: (l * w * (l + 1) * (w + 1)) / 4.
 */
 constexpr int countRectangles(int l, int w) {
 	return ((l * w * (l + 1) * (w + 1)) / 4);
