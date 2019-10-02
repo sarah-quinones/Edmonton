920a222f31d435e2206d5dd0fb213c2a6b25a88f
Rietty <rie.juliette@gmail.com>
2019-10-02 19:30:51 -0400

    Finished problem 78.

--- Solutions/51-100/78/Solution.cpp
+++ Solutions/51-100/78/Solution.cpp
@@ -12,21 +12,25 @@ Where p(0) = 1 and p(n) = 0 for n < 0..
 and k = m / 2 + 1 (k % 2 == 0) || -m / 2 + 1.
 */
 int main(int argc, char *argv[]) {
-	vector<cpp_int> partitions(100000);
-	partitions.push_back(1);
+	// We will need the generalized pentagonal numbers first.
+	vector<cpp_int> k;
+	k.push_back(1);
 	int n = 1;
+	// Loop through everything until we get something that fits our requirement.
 	while(true) {
-		int index = 0, gpenta = 1;
-		partitions.push_back(0);
-		while(gpenta <= n) {
-			int sign = (index % 4 > 1) ? -1 : 1;
-			partitions[n] += sign * partitions[n - gpenta];
-			partitions[n] %= 1000000;
-			index++;
-			int j = (!(index % 2)) ? index / 2 + 1 : (-index / 2 + 1);
-			gpenta = j * (3 * j - 1) / 2;
+		int i = 0;
+		int penta = 1;
+		k.push_back(0);
+		while(penta <= n) {
+			int sign = (i % 4 > 1) ? -1 : 1;
+			k[n] += sign * k[n - penta];
+			k[n] %= 1'000'000;
+			i++;
+			// Generate the next pentagonal (generalized number).
+			int j = (i % 2 == 0) ? i / 2 + 1 : -(i / 2 + 1);
+			penta = j * (3 * j - 1) / 2;
 		}
-		if(!partitions[n]) {
+		if(!k[n]) {
 			break;
 		}
 		n++;
